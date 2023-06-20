dee9b109f349a076b3b22d374c02660cba7f5510
JoseMaria <josmariaguilar@gmail.com>
2023-06-20 16:23:18 -0400

    implemented counter using useReducer hook

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 
 import './Counter.css'
 
+const initState = { count: 0 }
+
+const enum REDUCER_ACTION_TYPE {
+  INCREMENT,
+  DECREMENT
+}
+
+type ReducerAction = {
+  type: REDUCER_ACTION_TYPE
+}
+
+const reducer = (state: typeof initState, action: ReducerAction): typeof initState => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.INCREMENT:
+      return { ...state, count: state.count + 1 }
+
+    case REDUCER_ACTION_TYPE.DECREMENT:
+      return { ...state, count: state.count - 1 }
+
+    default:
+      throw new Error()
+  }
+}
+
 export const Counter = () => {
-  const [counter, setCounter] = useState(0)
+  const [state, dispatch] = useReducer(reducer, initState)
+
+  const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT })
+  const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT })
 
   return (
     <div className='container-counter'>
-      <h1>Counter: {counter}</h1>
+      <h1>Counter: {state.count}</h1>
       <nav>
-        <button onClick={() => setCounter(prev => prev + 1)}>Increment</button>
-        <button onClick={() => setCounter(prev => prev - 1)}>Decrement</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
       </nav>
     </div>
   )
